5c46b8e9982a583c5a5abdaa53b05c9763b9dc0f
Ross Younger <crazyscot@gmail.com>
2025-05-12 22:02:48 +1200

    chore: split out prettyprint code from manager.rs

--- qcp/src/config/manager.rs
+++ qcp/src/config/manager.rs
@@ -2,7 +2,7 @@
 // (c) 2024 Ross Younger
 
 use crate::{
-    cli::styles::{ColourMode, use_colours},
+    cli::styles::ColourMode,
     os::{AbstractPlatform as _, Platform},
     util::enums::DeserializableEnum,
 };
@@ -10,20 +10,13 @@ use crate::{
 use super::{Configuration, Configuration_Optional, ssh::ConfigFileError};
 
 use anyhow::Result;
-use figment::{Figment, Metadata, Provider, providers::Serialized, value::Value};
-use heck::ToUpperCamelCase;
+use figment::{Figment, Metadata, Provider, providers::Serialized};
 use serde::{Deserialize, de::Error};
 use std::{
-    collections::HashSet,
-    fmt::{Debug, Display},
+    fmt::Debug,
     path::{Path, PathBuf},
-    sync::LazyLock,
 };
 use struct_field_names_as_array::FieldNamesAsSlice;
-use tabled::{
-    Table, Tabled,
-    settings::{Color, Theme, object::Rows, style::Style},
-};
 
 use tracing::{debug, warn};
 
@@ -100,9 +93,9 @@ impl Provider for ClicolorEnv {
 #[derive(Debug)]
 pub struct Manager {
     /// Configuration data
-    data: Figment,
+    pub(super) data: Figment,
     /// The host argument this data was read for, if applicable
-    host: Option<String>,
+    pub(super) host: Option<String>,
 }
 
 impl Manager {
@@ -304,141 +297,6 @@ impl Manager {
         let working: Configuration_Optional = self.get::<Configuration_Optional>()?;
         working.validate()?;
         Ok(())
-    }
-}
-
-// PRETTY PRINT SUPPORT ///////////////////////////////////////////////////////////////////////////////////////
-
-static TABLE_STYLE: LazyLock<Theme> = LazyLock::new(|| {
-    if cfg!(windows) {
-        Style::psql().into()
-    } else {
-        Style::sharp().into()
-    }
-});
-
-/// Data type used when rendering the config table
-#[derive(Tabled)]
-struct PrettyConfig {
-    field: String,
-    value: String,
-    source: String,
-}
-
-impl PrettyConfig {
-    fn render_source(meta: Option<&Metadata>) -> String {
-        if let Some(m) = meta {
-            m.source
-                .as_ref()
-                .map_or_else(|| m.name.to_string(), figment::Source::to_string)
-        } else {
-            String::new()
-        }
-    }
-
-    fn render_value(value: &Value) -> String {
-        match value {
-            Value::String(_tag, s) => s.to_string(),
-            Value::Char(_tag, c) => c.to_string(),
-            Value::Bool(_tag, b) => b.to_string(),
-            Value::Num(_tag, num) => {
-                if let Some(i) = num.to_i128() {
-                    i.to_string()
-                } else if let Some(u) = num.to_u128() {
-                    u.to_string()
-                } else if let Some(ff) = num.to_f64() {
-                    ff.to_string()
-                } else {
-                    todo!("unhandled Num case");
-                }
-            }
-            Value::Empty(_tag, _) => "<empty>".into(),
-            // we don't currently support dict types
-            Value::Dict(_tag, _dict) => todo!(),
-            Value::Array(_tag, vec) => {
-                format!(
-                    "[{}]",
-                    vec.iter()
-                        .map(PrettyConfig::render_value)
-                        .collect::<Vec<_>>()
-                        .join(",")
-                )
-            }
-        }
-    }
-
-    fn new<F: Into<String>>(field: F, value: &Value, meta: Option<&Metadata>) -> Self {
-        Self {
-            field: field.into(),
-            value: PrettyConfig::render_value(value),
-            source: PrettyConfig::render_source(meta),
-        }
-    }
-}
-
-/// Pretty-printing type wrapper to Manager
-#[derive(Debug)]
-pub struct DisplayAdapter<'a> {
-    /// Data source
-    source: &'a Manager,
-    /// The fields we want to output. (If empty, outputs everything.)
-    fields: HashSet<String>,
-}
-
-impl Manager {
-    /// Creates a `DisplayAdapter` for this struct with the given options.
-    ///
-    /// # Returns
-    /// An ephemeral structure implementing `Display`.
-    #[must_use]
-    pub fn to_display_adapter<'de, T>(&self) -> DisplayAdapter<'_>
-    where
-        T: Deserialize<'de> + FieldNamesAsSlice,
-    {
-        let mut fields = HashSet::<String>::new();
-        fields.extend(T::FIELD_NAMES_AS_SLICE.iter().map(|s| String::from(*s)));
-        DisplayAdapter {
-            source: self,
-            fields,
-        }
-    }
-}
-
-impl Display for DisplayAdapter<'_> {
-    /// Formats the contents of this structure which are relevant to a given output type.
-    ///
-    /// N.B. This function uses CLI styling.
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = &self.source.data;
-
-        let mut output = Vec::<PrettyConfig>::new();
-        // First line of the table is special
-        let (host_string, host_colour) = if let Some(host) = &self.source.host {
-            (host.clone(), Color::FG_GREEN)
-        } else {
-            ("* (globals)".into(), Color::FG_CYAN)
-        };
-        output.push(PrettyConfig {
-            field: "(Remote host)".into(),
-            value: host_string,
-            source: String::new(),
-        });
-
-        let mut keys = self.fields.iter().collect::<Vec<_>>();
-        keys.sort();
-
-        for field in keys {
-            if let Ok(value) = data.find_value(field) {
-                let meta = data.get_metadata(value.tag());
-                output.push(PrettyConfig::new(field.to_upper_camel_case(), &value, meta));
-            }
-        }
-        let mut writable = Table::new(output);
-        let _ = writable.with(TABLE_STYLE.clone());
-        if use_colours() {
-            let _ = writable.modify(Rows::single(1), host_colour);
-        }
-        write!(f, "{writable}")
     }
 }
 
