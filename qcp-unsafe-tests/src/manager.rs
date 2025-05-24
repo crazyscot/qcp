4ac834b9bf6f8d970c71d663dfe57c2103e3cb3b
Ross Younger <crazyscot@gmail.com>
2025-05-24 15:14:39 +1200

    test: rewrite standard_reads_config (manager.rs) to use littertray

--- qcp-unsafe-tests/src/manager.rs
+++ qcp-unsafe-tests/src/manager.rs
@@ -3,51 +3,57 @@
 use qcp::Configuration;
 use qcp::config::Manager;
 
+use littertray::LitterTray;
 use rusty_fork::rusty_fork_test;
-use tempfile::TempDir;
 
-#[allow(unsafe_code)]
-pub(crate) unsafe fn fake_home() -> TempDir {
-    let tempdir = tempfile::tempdir().unwrap();
-    let fake_home = tempdir.path();
-
+unsafe fn prepare_fake_home(tray: &LitterTray) {
     // Temporarily override HOME environment variable
     // (this must only happen in single-threaded code)
+    let fake_home = tray.directory();
     #[allow(unsafe_code)]
     unsafe {
         std::env::set_var("HOME", fake_home);
     };
-    tempdir
-}
 
-rusty_fork_test! {
-    #[test]
-    fn standard_reads_config() {
-        #[allow(unsafe_code)]
-        let home = unsafe {
-            // SAFETY: this is run in a rusty-fork test, so it won't affect other tests
-            fake_home()
-        };
-        let test_conf = home.path().join(".qcp.conf");
-        let files = Manager::config_files();
-        assert!(files.contains(&test_conf.to_string_lossy().to_string()));
-        std::fs::write(&test_conf,
-            r"
+    tray.create_text(
+        ".qcp.conf",
+        r"
                 Host testhost
                 rx 1234
                 Host *
                 rx 2345
             ",
-        ).unwrap();
-        let mut mgr1 = Manager::standard(Some("testhost"));
-        mgr1.apply_system_default();
-        eprintln!("mgr1: {mgr1:?}");
-        assert!(mgr1.get::<Configuration>().unwrap().rx == 1234u64.into());
-        mgr1.validate_configuration().unwrap();
+    )
+    .unwrap();
+}
 
-        let mut mgr2 = Manager::standard(None);
-        mgr2.apply_system_default();
-        mgr2.validate_configuration().unwrap();
-        assert!(mgr2.get::<Configuration>().unwrap().rx == 2345u64.into());
+rusty_fork_test! {
+    #[test]
+    fn standard_reads_config_with_host() {
+        LitterTray::run(|tray| {
+            unsafe {
+                prepare_fake_home(tray);
+            }
+            let mut mgr1 = Manager::standard(Some("testhost"));
+            mgr1.apply_system_default();
+            //eprintln!("mgr1: {mgr1:?}");
+            assert!(mgr1.get::<Configuration>().unwrap().rx == 1234u64.into());
+            mgr1.validate_configuration().unwrap();
+        });
     }
+
+    #[test]
+    fn standard_reads_config_without_host() {
+        LitterTray::run(|tray| {
+            unsafe {
+                prepare_fake_home(tray);
+            }
+            let mut mgr1 = Manager::standard(None);
+            mgr1.apply_system_default();
+            //eprintln!("mgr1: {mgr1:?}");
+            assert!(mgr1.get::<Configuration>().unwrap().rx == 2345u64.into());
+            mgr1.validate_configuration().unwrap();
+        });
+    }
+
 }
