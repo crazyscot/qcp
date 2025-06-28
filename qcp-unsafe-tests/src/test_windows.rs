d4c4da54bf6d60342a08988b4001031c7334279f
Ross Younger <crazyscot@gmail.com>
2025-06-28 12:49:48 +1200

    test(windows): improve Windows config file path checks

--- qcp-unsafe-tests/src/test_windows.rs
+++ qcp-unsafe-tests/src/test_windows.rs
@@ -8,7 +8,7 @@ rusty_fork_test! {
     #[test]
     fn config_paths() {
         // It's tricky to assert much about these paths as we might be running on CI (Linux) or on Windows.
-        // So we use rusty_fork_test, and setenv.
+        // So we use rusty_fork_test, and on Unix we setenv to let them work.
         cfg_if! {
             if #[cfg(unix)] {
                 #[allow(unsafe_code)]
@@ -20,9 +20,9 @@ rusty_fork_test! {
             }
         }
 
-        assert!(Platform::system_ssh_config().unwrap().to_string_lossy().contains("/ssh_config"));
-        assert!(Platform::user_ssh_config().unwrap().to_string_lossy().contains("/config"));
-        assert!(Platform::system_config_path().unwrap().to_string_lossy().contains("/qcp.conf"));
+        assert!(Platform::system_ssh_config().unwrap().to_string_lossy().ends_with("/ssh_config"));
+        assert!(Platform::user_ssh_config().unwrap().to_string_lossy().ends_with("/config"));
+        assert!(Platform::system_config_path().unwrap().to_string_lossy().ends_with("/qcp.conf"));
 
         let pv = Platform::user_config_paths();
         assert!(pv.len() == 1);
