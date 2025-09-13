3f562061525bb3b5887e6a68b3c10ca938c94486
Ross Younger <crazyscot@gmail.com>
2025-09-14 01:15:30 +1200

    refactor: parsing for CredentialsType

--- qcp/src/util/mod.rs
+++ qcp/src/util/mod.rs
@@ -24,7 +24,7 @@ pub(crate) mod stats;
 pub(crate) mod time;
 
 pub(crate) mod serialization;
-pub use serialization::SerializeAsString;
+pub use serialization::{DeserializeEnum, SerializeAsString};
 
 mod tracing;
 pub(crate) use tracing::{
--- qcp/src/util/serialization.rs
+++ qcp/src/util/serialization.rs
@@ -1,10 +1,10 @@
 //! Serialization helpers
 // (c) 2024 Ross Younger
 
-use std::str::FromStr;
+use std::{marker::PhantomData, str::FromStr};
 
 use derive_more::with_trait::{Debug, Deref, Display};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer, de::Visitor};
 
 /// A wrapping helper type for enums that changes how they are serialized.
 ///
@@ -42,6 +42,67 @@ where
 impl<E: Clone + Display + FromStr> From<SerializeAsString<E>> for String {
     fn from(value: SerializeAsString<E>) -> Self {
         value.to_string()
+    }
+}
+
+/// String to enum deserialization helper trait (via enumscribe).
+///
+/// Where it is necessary for an enum to have multiple serializations
+/// (e.g. wire protocol and config file parsing), this trait can help.
+///
+/// If you implement this trait for an enum, then when the enum appears
+/// in a struct you can use the trait function in a serde attribute, like this:
+/// ```plaintext
+/// #[serde(deserialize_with = "CredentialsType::deserialize_str")]
+/// ```
+///
+/// The enum should auto-derive `enumscribe::TryUnscribe`, `strum::VariantNames`
+/// and `strum::AsRefStr`.
+///
+/// You are not expected to need to override the default implementation.
+pub trait DeserializeEnum
+where
+    Self: Sized + enumscribe::TryUnscribe + strum::VariantNames + AsRef<str>,
+{
+    /// String serialize function for an enum.
+    fn serialize_str<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.as_ref())
+    }
+
+    /// String deserialize function for an enum.
+    fn deserialize_str<'de, D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct MyVisitor<EE>(PhantomData<EE>);
+        impl<EE> Visitor<'_> for MyVisitor<EE>
+        where
+            EE: enumscribe::TryUnscribe + strum::VariantNames,
+        {
+            type Value = EE;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("a string")
+            }
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Self::Value::try_unscribe(v)
+                    .ok_or_else(|| serde::de::Error::unknown_variant(v, Self::Value::VARIANTS))
+            }
+        }
+        deserializer.deserialize_str(MyVisitor::<Self>(PhantomData))
+    }
+    /// String deserialize function for an `Option<some enum>`.
+    /// Always returns `Some(enum-type)`.
+    fn deserialize_str_optional<'de, D>(deserializer: D) -> Result<Option<Self>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(Some(Self::deserialize_str(deserializer)?))
     }
 }
 
