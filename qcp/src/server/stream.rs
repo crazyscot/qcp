8d451a61bb84f16c229476b28be2e0fcc293b0fc
Ross Younger <crazyscot@gmail.com>
2025-12-24 21:04:48 +1300

    test: refactor server/stream tests, add missing commands

--- qcp/src/server/stream.rs
+++ qcp/src/server/stream.rs
@@ -63,21 +63,21 @@ mod tests {
     use crate::protocol::{
         common::{ProtocolMessage, SendReceivePair},
         control::Compatibility,
-        session::{Command, GetArgs, PutArgs, Response, ResponseV1, Status},
+        session::{
+            Command, CreateDirectoryArgs, Get2Args, GetArgs, Put2Args, PutArgs, Response,
+            ResponseV1, SetMetadataArgs, Status,
+        },
     };
     use crate::util::io::DEFAULT_COPY_BUFFER_SIZE;
 
     use super::handle_stream;
+    use littertray::LitterTray;
     use tokio::io::simplex;
     use tokio_test::io::Builder;
 
-    #[tokio::test]
-    async fn test_handle_get_command() {
-        let get_cmd = Command::Get(GetArgs {
-            filename: "no-such-file.txt".to_string(),
-        });
+    async fn test_handler(cmd: Command, compat: u16) -> ResponseV1 {
         let mut send_buf = Vec::new();
-        get_cmd.to_writer_framed(&mut send_buf).unwrap();
+        cmd.to_writer_framed(&mut send_buf).unwrap();
 
         let mock_recv = Builder::new()
             .read(&send_buf[0..4])
@@ -88,7 +88,7 @@ mod tests {
 
         handle_stream(
             SendReceivePair::from((out_write, mock_recv)),
-            Compatibility::Level(1),
+            Compatibility::Level(compat),
             DEFAULT_COPY_BUFFER_SIZE,
         )
         .await
@@ -97,39 +97,67 @@ mod tests {
         let resp = Response::from_reader_async_framed(&mut out_read)
             .await
             .unwrap();
+        let Response::V1(r) = resp;
+        r
+    }
 
-        let Response::V1(ResponseV1 { status, .. }) = resp;
-        assert_eq!(status, Status::FileNotFound);
+    #[tokio::test]
+    async fn test_handle_get() {
+        let cmd = Command::Get(GetArgs {
+            filename: "no-such-file.txt".to_string(),
+        });
+        let resp = test_handler(cmd, 1).await;
+        assert_eq!(resp.status, Status::FileNotFound);
         // message is OS specific, so we don't check it here
     }
 
     #[tokio::test]
-    async fn test_handle_put_command() {
-        let put_cmd = Command::Put(PutArgs {
+    async fn test_handle_put() {
+        let cmd = Command::Put(PutArgs {
             filename: "/fjds/no-such-file.txt".to_string(),
         });
-        let mut send_buf = Vec::new();
-        put_cmd.to_writer_framed(&mut send_buf).unwrap();
+        let resp = test_handler(cmd, 1).await;
+        assert_eq!(resp.status, Status::DirectoryDoesNotExist);
+    }
 
-        let expected = Response::V1(ResponseV1::new(Status::DirectoryDoesNotExist.into(), None));
-        let mut expect_buf = Vec::new();
-        expected.to_writer_framed(&mut expect_buf).unwrap();
-
-        let mock_recv = Builder::new()
-            .read(&send_buf[0..4])
-            .read(&send_buf[4..])
-            .build();
-        let mock_send = Builder::new()
-            .write(&expect_buf[0..4])
-            .write(&expect_buf[4..])
-            .build();
-
-        let result = handle_stream(
-            SendReceivePair::from((mock_send, mock_recv)),
-            Compatibility::Level(1),
-            DEFAULT_COPY_BUFFER_SIZE,
-        )
-        .await;
-        assert!(result.is_ok());
+    #[tokio::test]
+    async fn handle_get2() {
+        let cmd = Command::Get2(Get2Args {
+            filename: String::from("/no-such-file"),
+            ..Default::default()
+        });
+        let resp = test_handler(cmd, 3).await;
+        assert_eq!(resp.status, Status::FileNotFound);
+    }
+    #[tokio::test]
+    async fn handle_put2() {
+        let cmd = Command::Put2(Put2Args {
+            filename: String::from("/blah/no-such-file"),
+            ..Default::default()
+        });
+        let resp = test_handler(cmd, 3).await;
+        assert_eq!(resp.status, Status::DirectoryDoesNotExist);
+    }
+    #[tokio::test]
+    async fn handle_mkdir() {
+        let resp = LitterTray::try_with_async(async |_| {
+            let cmd = Command::CreateDirectory(CreateDirectoryArgs {
+                dir_name: String::from("my_dir"),
+                ..Default::default()
+            });
+            Ok(test_handler(cmd, 3).await)
+        })
+        .await
+        .unwrap();
+        assert_eq!(resp.status, Status::Ok);
+    }
+    #[tokio::test]
+    async fn handle_setmeta() {
+        let cmd = Command::SetMetadata(SetMetadataArgs {
+            path: String::from("nosuchdir"),
+            ..Default::default()
+        });
+        let resp = test_handler(cmd, 3).await;
+        assert_eq!(resp.status, Status::FileNotFound);
     }
 }
